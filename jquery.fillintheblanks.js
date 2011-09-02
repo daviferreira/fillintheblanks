4e012d3b9ae58b6ce13aede1a3ccb8fa1ff58d06
Davi Ferreira <contato@daviferreira.com>
2011-09-02 15:44:55 -0300

    Refacotring.

--- jquery.fillintheblanks.js
+++ jquery.fillintheblanks.js
@@ -10,54 +10,58 @@
       result: 'result' 
     };
 
-    options = $.extend(defaults, options);
+    var options = $.extend(defaults, options || {});
+
+    var select_words = function(words){
+      var words_to_convert = []; 
+      var blanked_words = [];
+      var word, len;
+
+      while(words_to_convert.length < options.blanks_by_line && words_to_convert.length < words.length){
+        word = Math.floor(Math.random()*(words.length));
+        if(!already_blanked(word, blanked_words)){
+          len = words[word].length * options.size;
+          words_to_convert.push([word, len]);
+          blanked_words.push(word);
+        }
+      }
+      return words_to_convert;
+    };
+
+    var already_blanked = function(word, blanked_words){
+      for(x in blanked_words)
+        if(word == blanked_words[x])
+          return true;
+      return false;
+    };
+
+    var convert_words = function(words, words_to_convert){
+      for(w in words_to_convert){
+        var matches = words[words_to_convert[w][0]].match(/[\.!\?,]$/g);
+        words[words_to_convert[w][0]] = '';
+        if(matches && matches.length > 0)
+          words_to_convert[w][1] -= options.size;
+        for(var x = 1; x <= words_to_convert[w][1]; x++)
+          words[words_to_convert[w][0]] += options.symbol; 
+        if(matches && matches.length > 0)
+          words[words_to_convert[w][0]] += matches[0];
+      }
+      return words;
+    };
+
     return this.each(function(){
       $(this).submit(function(e){
         var original = $(this).find('textarea[name="'+options.original+'"]').val();
         var lines = original.split(/\n/);
-        var words, word, words_to_convert;
-        var len;
-        var already_picked, already_blanked;
         var result = '';
+
         for(i in lines){
-          words = lines[i].split(/ /);
-
+          var words = lines[i].split(/ /);
           if(words.length > 0){
-            words_to_convert = []; 
-            already_picked = [];
-
-            while(words_to_convert.length < options.blanks_by_line && words_to_convert.length < words.length){
-              word = Math.floor(Math.random()*(words.length));
-              already_blanked = false;
-              for(x in already_picked)
-                if(word == already_picked[x]){
-                  already_blanked = true;
-                  break;
-                }
-              if(!already_blanked){
-                len = words[word].length * options.size;
-                words_to_convert.push([word, len]);
-                already_picked.push(word);
-              }
-            }
-
-            for(w in words_to_convert){
-              var matches = words[words_to_convert[w][0]].match(/[\.!\?,]$/g);
-              words[words_to_convert[w][0]] = '';
-              if(matches && matches.length > 0)
-                words_to_convert[w][1] -= options.size;
-              for(var x = 1; x <= words_to_convert[w][1]; x++)
-                words[words_to_convert[w][0]] += options.symbol; 
-              if(matches && matches.length > 0)
-                words[words_to_convert[w][0]] += matches[0];
-            }
-
-            if(i > 0)
-              result += "\n";
-
-            for(w in words)
-              result += words[w] + " ";
-
+            var words_to_convert = select_words(words);
+            words = convert_words(words, words_to_convert);
+            if(i > 0) result += "\n";
+            for(w in words) result += words[w] + " ";
             result = result.replace(/ $/, "");
           }
         }
