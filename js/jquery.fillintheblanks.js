80ab533dc4f7f28b3c11642a639139a7cab1da4e
daviferreira <contato@daviferreira.com>
2011-09-03 11:33:34 -0300

    Added new functionalities to the project page.

--- js/jquery.fillintheblanks.js
+++ js/jquery.fillintheblanks.js
@@ -7,35 +7,50 @@
       symbol: '_',
       size: 1,
       original: 'original',
-      result: 'result' 
+      result: 'result',
+      ignore: [],
+      min_size: 2,
+      max_size: 0 
     };
 
     var options = $.extend(defaults, options || {});
 
-    var select_words = function(words){
+    for(word in options.ignore)
+      options.ignore[word] = options.ignore[word].toLowerCase();
+
+    var in_array = function(needle, haystack){
+      for(key in haystack)
+        if(needle == haystack[key])
+          return true;
+      return false;
+    };
+
+    var _is_valid_word = function(word){
+      return (!in_array(word.toLowerCase(), options.ignore) 
+              && word.length >= options.min_size 
+              && (options.max_size < 1 || word.length <= options.max_size)
+              && (isNaN(parseFloat(word))));
+    };
+
+    var _select_words = function(words){
       var words_to_convert = []; 
-      var blanked_words = [];
+      var checked_words = [];
       var word, len;
 
-      while(words_to_convert.length < options.blanks_by_line && words_to_convert.length < words.length){
+      while(words_to_convert.length < options.blanks_by_line && checked_words.length < words.length){
         word = Math.floor(Math.random()*(words.length));
-        if(!already_blanked(word, blanked_words)){
-          len = words[word].length * options.size;
-          words_to_convert.push([word, len]);
-          blanked_words.push(word);
+        if(!in_array(word, checked_words)){
+          checked_words.push(word);
+          if(_is_valid_word(words[word])){
+            len = words[word].length * options.size;
+            words_to_convert.push([word, len]);
+          }
         }
       }
       return words_to_convert;
     };
 
-    var already_blanked = function(word, blanked_words){
-      for(x in blanked_words)
-        if(word == blanked_words[x])
-          return true;
-      return false;
-    };
-
-    var convert_words = function(words, words_to_convert){
+    var _convert_words = function(words, words_to_convert){
       for(w in words_to_convert){
         var matches = words[words_to_convert[w][0]].match(/[\.!\?,]$/g);
         words[words_to_convert[w][0]] = '';
@@ -49,6 +64,13 @@
       return words;
     };
 
+    var _generate_line = function(line, words){
+      var new_line = '';
+      if(i > 0) new_line += "\n";
+      for(w in words) new_line += words[w] + " ";
+      return new_line.replace(/ $/, "");   
+    };
+
     return this.each(function(){
       $(this).submit(function(e){
         var original = $(this).find('textarea[name="'+options.original+'"]').val();
@@ -58,18 +80,15 @@
         for(i in lines){
           var words = lines[i].split(/ /);
           if(words.length > 0){
-            var words_to_convert = select_words(words);
-            words = convert_words(words, words_to_convert);
-            if(i > 0) result += "\n";
-            for(w in words) result += words[w] + " ";
-            result = result.replace(/ $/, "");
+            var words_to_convert = _select_words(words);
+            words = _convert_words(words, words_to_convert);
+            result += _generate_line(i, words);
           }
         }
 
         $(this).find('textarea[name="'+options.result+'"]').val(result);
         e.preventDefault();
       });
-
     });
 
   };
